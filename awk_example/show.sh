231d68554a8049cf7a669a5d5cc3652ced82e113
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2026-01-03 14:27:42 +0530

    made changes

--- awk_example/show.sh
+++ awk_example/show.sh
@@ -2,7 +2,7 @@
 #The comma after -F is the seperator. Vertically, It doesn't give a fuck what comes after the first ','
 awk -F"4" '{print var $1}' mock.csv
 #------------------------------------------------------------------------------------------
-awk -F',' -v var='dates_dawg:' '{print var $2}' mock.csv
+awk -F',' -v var='dates_dawg:' '{print var, $2}' mock.csv
 #OUTPUT:
 #dates_dawg:created
 #dates_dawg:2024-11-01
